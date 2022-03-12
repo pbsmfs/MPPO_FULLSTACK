17edb11a213b4893ad92b95b67e0f32b57ba9463
JuniorTheF <fsmart2006@gmail.com>
2022-03-12 16:58:31 +0300

    sensors migration and seed fixed + README updated

--- db/seeds/sensors.js
+++ db/seeds/sensors.js
@@ -2,7 +2,7 @@
 exports.seed = async function(knex) {
   await knex('data').del()
   await knex('data').insert([
-    {id: 1, 
+    {sensor_id: 1, 
       start_date: '09.12.2020 23:59:13', 
       end_date: '09.12.2020 23:59:13', 
       on_reactive_a: -600.0, 
@@ -19,7 +19,7 @@ exports.seed = async function(knex) {
       on_cos_c: 0.999,
       blocks_amount: 0
     },
-    {id: 2, 
+    {sensor_id: 1, 
       start_date: '10.12.2020 00:59:26', 
       end_date: '10.12.2020 00:59:26', 
       on_reactive_a: -600.0, 
