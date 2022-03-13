22c101c32356c583fd49660e94659810488f3114
JuniorTheF <fsmart2006@gmail.com>
2022-03-13 14:23:44 +0300

    step 2 express initial endpoints implemented

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser'
+import fetch from 'node-fetch'
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-app.use('/static', express.static('data'))
+app.use('/static', express.static('data'));
+
+app.get('/data/:id', (req, res) => { 
+    fetch(`http://localhost:${port}/static/${req.body.id}.csv`)
+    .then(response => response.text())
+    .then(response => res.send(response))
+    /*
+    TODO
+    data parsing
+    data inserting to db
+    */
+  }
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
