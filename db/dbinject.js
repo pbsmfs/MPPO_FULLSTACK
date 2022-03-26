abfba8a7594190ee036f2bd954a1d052b2530704
JuniorTheF <fsmart2006@gmail.com>
2022-03-26 15:26:12 +0300

    map through single csv done

--- db/dbinject.js
+++ db/dbinject.js
@@ -21,38 +21,41 @@ async function dbInject(id, knex) {
   let response = await fetch(`http://localhost:${port}/data/${id}`)
   let result = await response.json()
   console.log(result)
-  let data = {
-    sensor_id: id, 
-    start_date: result[0][0], 
-    end_date: result[0][1], 
-    on_reactive_a: parseFloat(result[0][2]),
-    on_reactive_b: parseFloat(result[0][3]),
-    on_reactive_c: parseFloat(result[0][4]),
-    on_active_a: parseFloat(result[0][5]),
-    on_active_b: parseFloat(result[0][6]),
-    on_active_c: parseFloat(result[0][7]),
-    on_voltage_a: parseInt(result[0][8]),
-    on_voltage_b: parseFloat(result[0][9]),
-    on_voltage_c: parseFloat(result[0][10]),
-    on_cos_a: parseFloat(result[0][11]),
-    on_cos_b: parseFloat(result[0][12]),
-    on_cos_c: parseFloat(result[0][13]),
-    off_reactive_a: parseFloat(result[0][14]),
-    off_reactive_b: parseFloat(result[0][15]),
-    off_reactive_c: parseFloat(result[0][16]),
-    off_active_a: parseFloat(result[0][17]),
-    off_active_b: parseFloat(result[0][18]),
-    off_active_c: parseFloat(result[0][19]),
-    off_voltage_a: parseFloat(result[0][20]),
-    off_voltage_b: parseFloat(result[0][21]),
-    off_voltage_c: parseFloat(result[0][22]),
-    off_cos_a: parseFloat(result[0][23]),
-    off_cos_b: parseFloat(result[0][24]),
-    off_cos_c: parseFloat(result[0][25]),
-    blocks_amount: parseFloat(result[0][26])
 
+  function dataStruct(ids) {
+    let data = {
+      sensor_id: id, 
+      start_date: ids[0], 
+      end_date: ids[1], 
+      on_reactive_a: parseFloat(ids[2]),
+      on_reactive_b: parseFloat(ids[3]),
+      on_reactive_c: parseFloat(ids[4]),
+      on_active_a: parseFloat(ids[5]),
+      on_active_b: parseFloat(ids[8]),
+      on_active_c: parseFloat(ids[7]),
+      on_voltage_a: parseInt(ids[8]),
+      on_voltage_b: parseFloat(ids[9]),
+      on_voltage_c: parseFloat(ids[10]),
+      on_cos_a: parseFloat(ids[11]),
+      on_cos_b: parseFloat(ids[12]),
+      on_cos_c: parseFloat(ids[13]),
+      off_reactive_a: parseFloat(ids[14]),
+      off_reactive_b: parseFloat(ids[15]),
+      off_reactive_c: parseFloat(ids[16]),
+      off_active_a: parseFloat(ids[17]),
+      off_active_b: parseFloat(ids[18]),
+      off_active_c: parseFloat(ids[19]),
+      off_voltage_a: parseFloat(ids[20]),
+      off_voltage_b: parseFloat(ids[21]),
+      off_voltage_c: parseFloat(ids[22]),
+      off_cos_a: parseFloat(ids[23]),
+      off_cos_b: parseFloat(ids[24]),
+      off_cos_c: parseFloat(ids[25]),
+      blocks_amount: parseFloat(result[0][26])
+    }
+    return data
   }
-  await knex.insert(data).into('data')
+  await knex.insert(result.map(item => dataStruct(item))).into('data')
 }
 
 dbInject(2, knex) // <- HARDCODED ID
